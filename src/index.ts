de8d18fcdbf7d0f556bf18a53dd2c83fb8de038d
Naphat <priasdevo@gmail.com>
2023-04-30 20:42:04 +0700

    feat: init index.ts

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+import dotenv from 'dotenv'
+import express from 'express'
+import cookieParser from 'cookie-parser'
+
+dotenv.config({ path: './config/.env' })
+const app = express()
+app.use(express.json())
+app.use(cookieParser())
+
+const PORT = process.env.PORT || 5000
+const server = app.listen(PORT, () => {
+  console.log('Server running in', process.env.NODE_ENV, 'mode on port', PORT)
+})
+
+// Handle promise rejection
+process.on('unhandledRejection', (err: Error, Promise) => {
+  console.log(`Error: ${err.message}`)
+  // close server
+  server.close(() => process.exit(1))
+})
